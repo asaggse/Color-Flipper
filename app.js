8c2fde9b4f43ae6c9a38f255a755bdafa7fef70f
Alessandro S <32938695+asaggse@users.noreply.github.com>
2020-10-01 11:49:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
-// const colors = ["green", "red", "rgba(133, 122, 200)", "#f15025"];
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 const btn = document.getElementById('btn');
 const color = document.querySelector(".color");
 const color2 = document.getElementById('text');
-
-// btn.addEventListener('click', function(){
-//     //get random number between 0 - 3
-//     const randomNumber = randomNumberGenerator();
-//     document.body.style.backgroundColor = colors[randomNumber];
-//     color.textContent = colors[randomNumber];
-// })
 
 btn.addEventListener('click', function(){
     let hexColor = '#';
@@ -20,7 +12,6 @@ btn.addEventListener('click', function(){
     document.body.style.backgroundColor = hexColor;
     color.textContent = hexColor;
 })
-
 
 function randomNumberGenerator() {
     // return Math.floor(Math.random() * colors.length);
